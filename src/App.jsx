483fb77b109ef2c0675dbf86545f7231e804f5ac
Sanya Bochoun <76147037+sanya-bochoun@users.noreply.github.com>
2025-03-18 14:11:29 +0700

    feat:Situation-1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-import "./App.css";
+/** @jsxImportSource @emotion/react */
+import { jsx } from '@emotion/react';
+import './App.css';
+import Button from './components/Button';
 
 function App() {
+  const sectionStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    margin: '10px'
+  };
+
+  const labelStyle = {
+    width: '100px',
+    marginRight: '20px'
+  };
+
+  const primaryCustomStyle = {
+    fontSize: '16px'
+  };
+
+  const secondaryCustomStyle = {
+    fontSize: '16px'
+  };
+
   return (
-    <div className="App">
-      <div className="button-components-section">
-        {/* Render ตัว Button 2 แบบ */}
-      </div>
-      <hr />
-      <div className="alert-components-section">
-        {/* Render ตัว Alert 4 แบบ */}
-      </div>
-    </div>
+    <main>
+      <section css={{...sectionStyle, marginBottom: '20px'}}>
+        <label css={labelStyle}>Primary</label>
+        <Button type="primary" css={primaryCustomStyle}>Button</Button>
+      </section>
+      <section css={sectionStyle}>
+        <label css={labelStyle}>Secondary</label>
+        <Button type="secondary" css={secondaryCustomStyle}>Button</Button>
+      </section>
+    </main>
   );
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,55 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { jsx } from '@emotion/react';
+import PropTypes from 'prop-types';
 
-function Button() {}
+function Button({ children, type = "primary", css }) {
+  const primaryStyle = {
+    backgroundColor: "#074EE8",
+    color: "white",
+    padding: "0 16px",
+    borderRadius: "4px",
+    border: "none",
+    cursor: "pointer",
+    fontWeight: "bold",
+    width: "171.19px",
+    height: "50px",
+    textAlign: "center",
+    gap: "8px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    ...css
+  };
+
+  const secondaryStyle = {
+    backgroundColor: "#07A4E8",
+    color: "white",
+    padding: "0 16px",
+    borderRadius: "4px",
+    border: "none",
+    cursor: "pointer",
+    fontWeight: "bold",
+    width: "171.19px",
+    height: "50px",
+    textAlign: "center",
+    gap: "8px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    ...css
+  };
+
+  return (
+    <button css={type === "primary" ? primaryStyle : secondaryStyle}>
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  children: PropTypes.node,
+  type: PropTypes.oneOf(['primary', 'secondary']),
+  css: PropTypes.object
+};
+
+export default Button;
