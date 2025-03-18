a63d4bb3742bf5f2139372bc668dd6dce7e94a2f
Jin react app <Passawit14@gmail.com>
2025-03-18 14:57:03 +0700

    finished

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,36 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import {css} from "@emotion/react"
 
-function Alert() {}
+function Alert({type}) {
+
+const errorBox =css`
+
+background-color: #721c24;
+  
+  `
+const warningBox = css`
+
+background-color: #856404;
+  
+`
+const infoBox = css`
+
+background-color: #0c5460;
+  
+`
+const successBox = css`
+
+background-color: #155724;
+  
+`
+return(
+    <h1 css={
+        type === "error" ? errorBox : 
+        type === "warning" ? warningBox : 
+        type === "info" ? infoBox : 
+        type === "success" ? successBox : 
+        null}> 
+        this is {type} alert  </h1>
+)
+}
+export default Alert
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,33 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import {css} from "@emotion/react"
 
-function Button() {}
+function Button({type}) {
+    const primaryStyle = css`
+    background: rgba(7, 78, 232, 1);
+    width: 171.18959045410156;
+    height: 50;
+    top: 291px;
+    left: 278.92px;
+    border-radius: 4px;
+    padding-right: 16px;
+    padding-left: 16px;
+    gap: 8px;
+    `
+    const secondaryStyle = css`
+    width: 171.18959045410156;
+    height: 50;
+    top: 381px;
+    left: 277.78px;
+    border-radius: 4px;
+    padding-right: 16px;
+    padding-left: 16px;
+    gap: 8px;
+    background:rgba(7, 164, 232, 1)
+    `
+;
+    
+    return(
+        <h1 css={type === "primary" ? primaryStyle : secondaryStyle }>Button</h1>
+    )
+}
+export default Button
